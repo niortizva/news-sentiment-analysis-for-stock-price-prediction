2c7e09dee1d084748aa9dcaeaeac86a9c51d226d
niortizva <33683379+niortizva@users.noreply.github.com>
2026-05-30 20:56:32 +0000

    streamlit usage optimization

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
 from src.data_insights import load_data
-from src.data_loading import align_news_to_price
-from src.feature_engineering import prepare_features, scale_features, get_llm_embeddings
+# from src.data_loading import align_news_to_price
+# from src.feature_engineering import prepare_features, scale_features, get_llm_embeddings
 from src.model import load_model
 import joblib
 
@@ -14,16 +15,15 @@ def predict_future(ticker, current_date, days_ahead=[1, 3, 5, 10],
     Predict future returns for a specific ticker at a specific date
     """
     # Load latest data
-    df_price, df_news = load_data()
-    
+    df_price, _ = load_data()
     # Align and prepare
-    df_aligned = align_news_to_price(df_price, df_news)
-    
+    # df_aligned = align_news_to_price(df_price, df_news)
     # Add embeddings (in production, you'd cache or use a lighter model)
-    df_with_emb = get_llm_embeddings(df_aligned)
-    
+    # df_with_emb = get_llm_embeddings(df_aligned)
     # Prepare features
-    df_ready, numeric_cols = prepare_features(df_with_emb)
+    # df_ready, numeric_cols = prepare_features(df_with_emb)
+
+    df_ready, numeric_cols = joblib.load('models/prepared_data.pkl')  # Load preprocessed data and numeric columns
     
     # Filter for specific ticker and date
     target_date = pd.to_datetime(current_date).normalize()
@@ -34,7 +34,7 @@ def predict_future(ticker, current_date, days_ahead=[1, 3, 5, 10],
     
     if len(feature_row) == 0:
         # raise ValueError(f"No data for {ticker} on {current_date}")
-        raise ValueError("**Select prediction date (Trading days only - Mon to Fri)**")
+        raise ValueError("Select prediction date (Trading days only - Mon to Fri)")
     
     # Load model and scaler
     model = load_model(model_path)
